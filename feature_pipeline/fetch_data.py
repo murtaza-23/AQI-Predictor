670bc80e87a0902b8c04cf33b52b97f247b949fd
murtaza-23 <murtazacolombo5253@gmail.com>
2026-08-08 22:09:54 +0500

    Fetching data from Open-Mateo and OpenWeather APIs

--- feature_pipeline/fetch_data.py
+++ feature_pipeline/fetch_data.py
@@ -14,8 +14,10 @@ AQICN_API_KEY = os.getenv("AQICN_API_KEY")
 
 LAT, LON = 24.860753, 67.029503 # latitude and longitude coordinates for my current location (Karachi)
 
+# API Endpoints
 ow_base_url = "http://api.openweathermap.org/data/2.5"
 aqicn_base_url = "https://api.waqi.info/feed"
+open_mateo_base_url = "https://air-quality-api.open-meteo.com/v1/air-quality"
 
 def fetch_aqicn_data():
     url = f"{aqicn_base_url}/Karachi"
@@ -33,6 +35,28 @@ def fetch_aqicn_data():
 
     return {
         "aqi": data["aqi"],    
+    }
+
+def fetch_open_mateo_data():
+    params = {"latitude": LAT, "longitude": LON, "current": "us_aqi", "timezone": "Asia/Karachi"}
+
+    response = requests.get(open_mateo_base_url, params=params)
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    current = data.get("current", {})
+
+    aqi = current["us_aqi"]
+
+    if aqi is None:
+        raise ValueError(
+            "Open-Meteo did not return a current US AQI value."
+        )
+
+    return {
+        "aqi": int(round(aqi))
     }
 
 def fetch_openweather_weather_data():
@@ -74,13 +98,13 @@ def fetch_openweather_air_pollution_data():
     }
 
 def fetch_all_data():
-    aqicn = fetch_aqicn_data()
+    om_aqi = fetch_open_mateo_data()
     weather = fetch_openweather_weather_data()
     pollution = fetch_openweather_air_pollution_data()
 
     merged = {
         "timestamp": datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S"),
-        "aqi": aqicn["aqi"],
+        "aqi": om_aqi["aqi"],
         "pm2_5": pollution["pm2_5"],
         "pm10": pollution["pm10"],
         "o3": pollution["o3"],
@@ -94,7 +118,7 @@ def fetch_all_data():
 
 if __name__ == "__main__":
     data = fetch_all_data()
-    print(f"\nCombined Data (AQICN and OpenWeather)\n")
+    print(f"\nCombined Data (Open-Mateo and OpenWeather)\n")
     for k, v in data.items():
         print(f"{k}: {v}")
 

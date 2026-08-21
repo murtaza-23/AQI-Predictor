239c11d9e461b3752cd0d1b00c57897180e00b16
murtaza-23 <murtazacolombo5253@gmail.com>
2026-08-21 18:08:25 +0500

    implemented AQI model training pipeline

--- training_pipeline/train.py
+++ training_pipeline/train.py
@@ -83,12 +83,14 @@ def load_data() -> pd.DataFrame:
 
         fs = project.get_feature_store()
 
+        fs.engine = "hive" 
+
         fg = fs.get_feature_group(
             name="aqi_features",
             version=1
         )
       
-        df = fg.read(arrow_flight=False)
+        df = fg.read()
 
     df["timestamp"] = pd.to_datetime(df["timestamp"])
 
